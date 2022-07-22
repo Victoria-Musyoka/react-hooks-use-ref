ece5a09f492912a542ab7c65112fc14ad119296b
Victoria Musyoka <victoria.musyoka@student.moringaschool.com>
2022-07-22 14:02:36 +0300

    complete

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,10 +3,15 @@ import React, { useRef } from "react";
 function Box() {
   const elementRef = useRef();
 
+  const handleMeasureClick = () => {
+    const div = elementRef.current;
+    console.log("Measuerments:", div.getBoundingClientRect());
+  };
+
   return (
     <div ref={elementRef}>
       <h1>Box</h1>
-      <button>Measure</button>
+      <button onClick={handleMeasureClick}>Measure</button>
     </div>
   );
 }
